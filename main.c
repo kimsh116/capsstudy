6fae2cdceb6f760cef4c57ae0524329348719888
kkaebi <dsksh0602@naver.com>
2024-03-27 19:36:15 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARINGS
+
+#include <stdio.in>
+
+int main(void) {
+	int a;
+	printf("숫자 하나를 입력하세요 : ");
+	scanf("%d", &a);
+
+	printf("입력한 숫자는 %d입니다.\n", a);
+
+	return 0;
+}
